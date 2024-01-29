a71e9a7d592afd834c8345a326a5fe977f78fac2
Jamie Crosby <taladan@gmail.com>
2024-01-29 14:08:01 -0500

    testing: Add test for string and symbol usage
    Test the code for Board#get to see if it passes when either a string or
    a symbol is passed in for retrieving the square information

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,8 +2,13 @@ require './lib/board'
 
 describe Board do
   describe "#get" do
-    it "returns a square object" do
+    it "returns a square object from a string" do
       square = Board.new.get("A1")
+      expect(square).to be_a(Square)
+    end
+
+    it "returns a square object from a symbol" do
+      square = Board.new.get(:g4)
       expect(square).to be_a(Square)
     end
   end
