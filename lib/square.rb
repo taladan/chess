21079c04d740f1d1493ce477ad6c540d6f906be5
Jamie Crosby <taladan@gmail.com>
2024-01-29 20:14:21 -0500

    cleanup: fix whitespace and comment issues

--- lib/square.rb
+++ lib/square.rb
@@ -1,11 +1,17 @@
+# frozen_string_literal: true
+
+# Square object
+# - can hold a piece object 
+# - has a color attribute 
+# - can tell if it is occupied
 class Square
   attr_reader :color, :piece
-  
+
   def initialize(color, piece=nil)
     @color = color
     @piece = piece
   end
-  
+
   def occupied?
     !@piece.nil?
   end
