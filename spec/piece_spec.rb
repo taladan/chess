c836c2ee9cad169865defaa5fd8117e7b47666d0
Jamie Crosby <taladan@gmail.com>
2024-01-30 10:30:19 -0500

    cleanup: remove unnecessary test
    Piece no longer takes +name+

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -1,32 +1,25 @@
 # frozen_string_literal: true
 
-require './lib/piece'
+require 'piece'
 
 describe Piece do
-  describe '#name' do
-    it 'returns the piece name when queried' do
-      piece = Piece.new(:white, 'pawn')
-      expect(piece.name).to eq('pawn')
-    end
-  end
-
   describe '#color' do
     it 'returns the piece color when queried' do
-      piece = Piece.new(:white, 'rook')
+      piece = Piece.new(:white)
       expect(piece.color).to eq(:white)
     end
   end
 
   describe '#current_square' do
     it 'returns a symbol' do
-      current_square = Piece.new(:white,'rook').current_square
+      current_square = Piece.new(:white).current_square
       expect(current_square).to be_a(Symbol)
     end
   end
 
   describe '#possible_moves' do
     it 'returns an array' do
-      possible_moves = Piece.new(:black,'queen').possible_moves
+      possible_moves = Piece.new(:black).possible_moves
       expect(possible_moves).to be_an(Array)
     end
   end
