2b766e59685157136efb234d19e796e580ec3517
Jamie Crosby <taladan@gmail.com>
2024-01-29 19:13:45 -0500

    feature: Piece class initial commit
    implement base attributes of a chess Piece

---
+++ lib/piece.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# parent class for chess pieces
+class Piece
+  attr_reader :color, :name, :possible_moves
+  attr_accessor :current_square
+
+  def initialize(color, name)
+    @color = color
+    @name = name
+    @current_square = :tray
+    @possible_moves = []
+  end
+end
