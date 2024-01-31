bdc4a7cbd282a41bc356094e854bcccc58f430aa
Jamie Crosby <taladan@gmail.com>
2024-01-31 13:17:26 -0500

    testing: refactor to reflect #possible_moves output
    After talking with some folks on The Odin Project, I realized that I
    need to be testing for what values I'm supposed to be getting.

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -3,20 +3,6 @@
 require 'pawn'
 
 describe Pawn do
-  describe '#color' do
-    it 'returns the piece color when queried' do
-      piece = Pawn.new(:white)
-      expect(piece.color).to eq(:white)
-    end
-  end
-
-  describe '#current_square' do
-    it 'returns a symbol' do
-      current_square = Pawn.new(:white).current_square
-      expect(current_square).to be_a(Symbol)
-    end
-  end
-
   describe '#possible_moves' do
     it 'returns an array' do
       possible_moves = Pawn.new(:black).possible_moves
@@ -25,35 +11,39 @@ describe Pawn do
   end
 
   describe '#possible_moves' do
-    it 'when a pawn is black and has not moved, its return array includes [0,-2]' do
+    it 'when a pawn is black and has not moved it returns the expected array' do
       piece = Pawn.new(:black)
-      possible_moves = piece.possible_moves.include?([0,-2])
-      expect(possible_moves).to be(true)
+      expected = [[0, -1], [0, -2], [1, -1], [-1, -1]]
+      possible_moves = piece.possible_moves
+      expect(possible_moves).to eq(expected)
     end
   end
 
   describe '#possible_moves' do
-    it 'when a pawn is black and has moved its return array does not include [0,-2]' do
+    it 'when a pawn is black and has moved it returns the expected array' do
       piece = Pawn.new(:black)
+      expected = [[0, -1], [1, -1], [-1, -1]]
       piece.already_moved = true
-      possible_moves = piece.possible_moves.include?([0,-2])
-      expect(possible_moves).to be(false)
+      possible_moves = piece.possible_moves
+      expect(possible_moves).to eq(expected)
     end
   end
 
   describe '#possible_moves' do
-    it 'when a pawn is white and has not moved, its return array includes [0,2]' do
-      possible_moves = Pawn.new(:white).possible_moves.include?([0,2])
-      expect(possible_moves).to be(true)
+    it 'when a pawn is white and has not moved it returns the expected array' do
+      possible_moves = Pawn.new(:white).possible_moves
+      expected = [[0, 1], [0, 2], [1, 1], [-1, 1]]
+      expect(possible_moves).to eq(expected)
     end
   end
  
   describe '#possible_moves' do
-    it 'when a pawn is white and has moved, its return array does not include [0,2]' do
+    it 'when a pawn is white and has moved it returns the expected array' do
       piece = Pawn.new(:white)
+      expected = [[0, 1], [1, 1], [-1, 1]]
       piece.already_moved = true
-      possible_moves = piece.possible_moves.include?([0,2])
-      expect(possible_moves).to be(false)
+      possible_moves = piece.possible_moves
+      expect(possible_moves).to eq(expected)
     end
   end
 end
