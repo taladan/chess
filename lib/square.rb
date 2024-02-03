823cc4557ba3eba457b79d968691783fb85a5986
Jamie Crosby <taladan@gmail.com>
2024-02-02 20:11:53 -0500

    fix: make @piece accessible
    Need to be able to set @pieces values

--- lib/square.rb
+++ lib/square.rb
@@ -5,7 +5,8 @@
 # - has a color attribute 
 # - can tell if it is occupied
 class Square
-  attr_reader :color, :piece
+  attr_reader :color
+  attr_accessor :piece
 
   def initialize(color, piece=nil)
     @color = color
