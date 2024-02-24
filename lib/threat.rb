d37d90217e155f0b6fb5ecbce788793cb924adeb
Jamie Crosby <taladan@gmail.com>
2024-02-23 22:26:23 -0500

    fix: migrate threatened squares calcs from piece
    Realized these methods live better in Threat

--- lib/threat.rb
+++ lib/threat.rb
@@ -7,25 +7,31 @@ class Threat
   end
 
   # return hash of all threatened positions, keyed by piece
-  def all_threats
-    squares_threatened_by_black_pieces.merge(squares_threatened_by_white_pieces)
+  def all_threats(attack=true)
+    @attack = attack
+    squares_threatened_by_black_pieces(@attack).merge(squares_threatened_by_white_pieces(@attack))
   end
 
   # return hash of all positions threatened by black pieces
-  def squares_threatened_by_black_pieces
+  def squares_threatened_by_black_pieces(attack=true)
+    @attack = attack
     threat_by_color(:black)
   end
 
   # return hash of all squares threatened by white pieces
-  def squares_threatened_by_white_pieces
+  def squares_threatened_by_white_pieces(attack=true)
+    @attack = attack
     threat_by_color(:white)
   end
 
   private
+
   # return array of positions on board threatened by piece
   def get_squares_threatened_by_piece(piece)
     # expects +piece+ to be a Piece object
-    piece.threatened_squares.map { |square| square if @board.on_board?(square) }.compact
+    threatened_squares(piece).map do |square|
+      square if @board.on_board?(square)
+    end.compact
   end
 
   # return hash of threatened squares
@@ -37,5 +43,28 @@ class Threat
       threat_matrix[piece] = get_squares_threatened_by_piece(piece)
     end
     threat_matrix
+  end
+
+  # return array of threatened positions
+  def threatened_squares(piece)
+    return pawn_threats(piece) if piece.pawn? && @attack
+
+    moves = []
+    piece.possible_moves.each do |move|
+      moves.push(piece.current_square.relative_position(**move))
+    end
+    moves
+  end
+
+  # remove vertical moves and return array of threatened positions
+  def pawn_threats(piece)
+    # code here
+    moves = []
+    piece.possible_moves.each do |move|
+      next if move.length == 1
+
+      moves.push(piece.current_square.relative_position(**move))
+    end
+    moves
   end
 end
