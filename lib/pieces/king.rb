326a5bacc191342ac960629ec86f304da0c9abf1
D.J. Crosby <taladan@gmail.com>
2024-07-30 11:32:35 -0400

    feat: castling allows more movement from kings
    castling rule moves king 2 spaces in lateral direction

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 
-require_relative './piece.rb'
+require_relative './piece'
 
 # King chess piece
 class King < Piece
-
-  def possible_moves 
-    [
-      {up: 1},
-      {left: 1},
-      {right: 1},
-      {down: 1},
-      {up: 1, left: 1},
-      {up:1, right: 1},
-      {down: 1, left: 1},
-      {down:1, right: 1},
+  def possible_moves
+    moveset = [
+      { up: 1 },
+      { left: 1 },
+      { right: 1 },
+      { down: 1 },
+      { up: 1, left: 1 },
+      { up: 1, right: 1 },
+      { down: 1, left: 1 },
+      { down: 1, right: 1 }
     ]
+    # Here we add :left 2 and :right 2 if the king hasn't moved for Castling rule
+    moveset.concat([left: 2], [right: 2]) unless @already_moved
+    moveset.flatten
   end
 end
