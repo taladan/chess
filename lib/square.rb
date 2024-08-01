c567da966f94b8910879481039cc4c72dfa5ebf5
D.J. Crosby <taladan@gmail.com>
2024-08-01 19:36:11 -0400

    Typo

--- lib/square.rb
+++ lib/square.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 # Square object
-# - can hold a piece object 
-# - has a color attribute 
+# - can hold a piece object
+# - has a color attribute
 # - can tell if it is occupied
 class Square
   attr_reader :color
   attr_accessor :piece, :rank, :file, :position
 
-  def initialize(color, piece=nil)
+  def initialize(color, piece = nil)
     @color = color
     @piece = piece
     @position = nil
