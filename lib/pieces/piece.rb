3607a5aa5f368b55b08cf68c6650a03d7027e8ea
D.J. Crosby <taladan@gmail.com>
2024-08-10 19:44:34 -0400

    docs: Remove unnecessary comments

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 # parent class for chess pieces
-# Methods:
-# #possible_moves - return array of possible moves based on piece movement
-#                   algorithm
 class Piece
   attr_reader :color
   attr_accessor :current_square
